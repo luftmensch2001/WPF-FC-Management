4dd272ce66820c3aca812dd0dc07bd2fec54fb7d
luftmensch2001 <phuongit1803@gmail.com>
2021-09-29 10:04:41 +0700

    Create base model

---
+++ FCM/ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace FCM.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+           
+        }
+
+    }
+}
