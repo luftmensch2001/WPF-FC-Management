b733cc501363a4e301826a1b06d3708b5b509199
luftmensch2001 <phuongit1803@gmail.com>
2021-10-03 22:06:36 +0700

    EditMatchInfor window

--- FCM/obj/Debug/netcoreapp3.1/UserControls/ucCard.g.i.cs
+++ FCM/obj/Debug/netcoreapp3.1/UserControls/ucCard.g.i.cs
@@ -95,7 +95,7 @@ namespace FCM.UserControls {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/FCM;V1.0.0.0;component/usercontrols/uccard.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FCM;component/usercontrols/uccard.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\UserControls\ucCard.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
