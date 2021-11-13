a86f0276be8769f81033fd689e63d8982798b161
luftmensch2001 <phuongit1803@gmail.com>
2021-11-13 09:02:34 +0700

    Penalty Window

--- FCM/ViewModel/MainViewModel.cs
+++ FCM/ViewModel/MainViewModel.cs
@@ -76,9 +76,6 @@ namespace FCM.ViewModel
         }
         public void SwitchTab(MainWindow parameter)
         {
-            ResultRecordingWindow wd = new ResultRecordingWindow();
-            wd.ShowDialog();
-
             int index = int.Parse(uid); // tab index
             //Move Stroke Menu
             parameter.grdStroke.Margin = new Thickness(0, (150 + 60 * index), 0, 0);
